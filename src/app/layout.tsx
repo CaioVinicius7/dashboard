f770c52aaed786434be1505f34fc44c460e5bd6c
Caio Vinícius <caio1525pereira@gmail.com>
2024-05-30 18:29:27 -0300

    feat: :tada: Configuração incial do projeto

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Dashboard"
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
