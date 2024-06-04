e7a10ac1a53bbeb47215d622b50061b3ddc07ec0
Caio Vinícius <caio1525pereira@gmail.com>
2024-06-03 22:33:48 -0300

    feat: :lipstick: Base da página de funcionários

---
+++ src/app/(dashboard)/employees/page.tsx
@@ -0,0 +1,124 @@
+import { Pencil, Trash2, UserPlus } from "lucide-react";
+
+import { Header } from "@/components/Header";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from "@/components/ui/table";
+
+export default function Employees() {
+  return (
+    <>
+      <Header title="Funcionários" />
+
+      <main className="space-y-4 p-4">
+        <div>
+          <Button variant="outline" className="ml-auto flex items-center gap-2">
+            <UserPlus className="size-6" />
+            Adicionar
+          </Button>
+        </div>
+
+        <div>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Nome</TableHead>
+                <TableHead className="w-[100px]">Cargo</TableHead>
+                <TableHead className="w-[150px]">Contato</TableHead>
+                <TableHead className="w-[175px]">Data de ingressão</TableHead>
+                <TableHead className="w-[150px]">Salário</TableHead>
+                <TableHead className="w-[150px]" />
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">
+                  Ellen Cristina Pimentel Diógenes de Carvalho
+                </TableCell>
+                <TableCell>Serrador</TableCell>
+                <TableCell>(12) 9 4002-8922</TableCell>
+                <TableCell>03/06/2024</TableCell>
+                <TableCell>R$ 1250.00</TableCell>
+
+                <TableCell className="flex items-center gap-2">
+                  <Button
+                    variant="ghost"
+                    className="flex items-center justify-center"
+                  >
+                    <Pencil className="size-5" />
+                  </Button>
+
+                  <Button
+                    variant="ghost"
+                    className="flex items-center justify-center"
+                  >
+                    <Trash2 className="size-5" />
+                  </Button>
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell className="font-medium">
+                  Ellen Cristina Pimentel Diógenes de Carvalho
+                </TableCell>
+                <TableCell>Serrador</TableCell>
+                <TableCell>(12) 9 4002-8922</TableCell>
+                <TableCell>03/06/2024</TableCell>
+                <TableCell>R$ 1250.00</TableCell>
+
+                <TableCell className="flex items-center gap-2">
+                  <Button
+                    variant="ghost"
+                    className="flex items-center justify-center"
+                  >
+                    <Pencil className="size-5" />
+                  </Button>
+
+                  <Button
+                    variant="ghost"
+                    className="flex items-center justify-center"
+                  >
+                    <Trash2 className="size-5" />
+                  </Button>
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell className="font-medium">
+                  Ellen Cristina Pimentel Diógenes de Carvalho
+                </TableCell>
+                <TableCell>Serrador</TableCell>
+                <TableCell>(12) 9 4002-8922</TableCell>
+                <TableCell>03/06/2024</TableCell>
+                <TableCell>R$ 1250.00</TableCell>
+
+                <TableCell className="flex items-center gap-2">
+                  <Button
+                    variant="ghost"
+                    className="flex items-center justify-center"
+                  >
+                    <Pencil className="size-5" />
+                  </Button>
+
+                  <Button
+                    variant="ghost"
+                    className="flex items-center justify-center"
+                  >
+                    <Trash2 className="size-5" />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { ModeToggle } from "./themeToggle";
+
+export interface HeaderProps {
+  title: string;
+}
+
+export function Header({ title }: HeaderProps) {
+  return (
+    <header className="flex items-center justify-between border-b p-4">
+      <h2 className="text-xl">{title}</h2>
+
+      <ModeToggle />
+    </header>
+  );
+}
