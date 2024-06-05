48ca3227ab3b992eb2117766425500a1d18a6033
Caio Vinícius <caio1525pereira@gmail.com>
2024-06-05 12:00:16 -0300

    feat: :sparkles: Função utilitária currencyStringToNumber

---
+++ src/utils/currencyStringToNumber.ts
@@ -0,0 +1,11 @@
+export function currencyStringToNumber(value: string | number) {
+  if (typeof value === "number") {
+    return value;
+  }
+  const sanitizedString = value
+    .replace(/\./g, "")
+    .replace(",", ".")
+    .replace("R$", "");
+
+  return Number(sanitizedString);
+}
