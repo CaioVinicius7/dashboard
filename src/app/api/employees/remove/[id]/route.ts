d69d2a673a9988b0c0e78e3d72712dfc16cbdb89
Caio Vinícius <caio1525pereira@gmail.com>
2024-06-05 23:03:44 -0300

    feat: :sparkles: Endpoint de remoção de funcionário

---
+++ src/app/api/employees/remove/[id]/route.ts
@@ -0,0 +1,66 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { z, ZodError } from "zod";
+
+import { prisma } from "@/lib/prisma";
+
+interface Params {
+  id: string;
+}
+
+const removeEmployeeParamsSchema = z.object({
+  id: z.string()
+});
+
+export async function DELETE(req: NextRequest, { params }: { params: Params }) {
+  try {
+    const { id } = removeEmployeeParamsSchema.parse(params);
+
+    const employeeFromId = await prisma.employee.findUnique({
+      where: {
+        id
+      }
+    });
+
+    if (!employeeFromId) {
+      return NextResponse.json(
+        {
+          message: "Employee not found."
+        },
+        {
+          status: 400
+        }
+      );
+    }
+
+    await prisma.employee.delete({
+      where: {
+        id
+      }
+    });
+
+    return new NextResponse(null, {
+      status: 204
+    });
+  } catch (error: any) {
+    if (error instanceof ZodError) {
+      return NextResponse.json(
+        {
+          message: "Validation error.",
+          errors: error.flatten().fieldErrors
+        },
+        {
+          status: 400
+        }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        message: error.message
+      },
+      {
+        status: 500
+      }
+    );
+  }
+}
