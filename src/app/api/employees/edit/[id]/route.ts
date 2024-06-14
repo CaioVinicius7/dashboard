a13c43c6c6ed323ca67dd14081205b5b426bd033
Caio Vinícius <caio1525pereira@gmail.com>
2024-06-14 18:31:11 -0300

    feat: :sparkles: Endpoint de edição de funcionário

---
+++ src/app/api/employees/edit/[id]/route.ts
@@ -0,0 +1,130 @@
+import { parse } from "date-fns";
+import { type NextRequest, NextResponse } from "next/server";
+import { z, ZodError } from "zod";
+
+import { prisma } from "@/lib/prisma";
+import { ROLES_IN_DB } from "@/utils/constants";
+
+interface Params {
+  id: string;
+}
+
+const rolesSchema = z.enum(ROLES_IN_DB, {
+  required_error: "Cargo inexistente."
+});
+
+const editEmployeeParamsSchema = z.object({
+  id: z.string()
+});
+
+const editEmployeeBodySchema = z.object({
+  name: z
+    .string()
+    .min(1, "O campo nome precisa ter no mínimo 1 caractere")
+    .optional(),
+  role: rolesSchema.optional(),
+  phone: z
+    .string()
+    .min(16, "O campo telefone precisa ser preenchido corretamente")
+    .optional(),
+  entryDate: z
+    .string()
+    .min(10, "O campo data de ingressão precisa ser preenchido corretamente")
+    .optional(),
+  salary: z
+    .number({
+      required_error: "O campo salário precisa ser informado",
+      invalid_type_error: "O campo salário precisa ser do tipo numérico"
+    })
+    .optional()
+});
+
+export async function PUT(req: NextRequest, { params }: { params: Params }) {
+  try {
+    const { id } = editEmployeeParamsSchema.parse(params);
+
+    const body = await req.json();
+
+    const { name, role, phone, entryDate, salary } =
+      editEmployeeBodySchema.parse(body);
+
+    const employeeFromId = await prisma.employee.findUnique({
+      where: {
+        id
+      }
+    });
+
+    if (!employeeFromId) {
+      return NextResponse.json(
+        {
+          message: "Funcionário não encontrado."
+        },
+        {
+          status: 400
+        }
+      );
+    }
+
+    if (!!phone) {
+      const employeeFromPhone = await prisma.employee.findUnique({
+        where: {
+          phone,
+          NOT: {
+            id: employeeFromId.id
+          }
+        }
+      });
+
+      if (!!employeeFromPhone) {
+        return NextResponse.json(
+          {
+            message: "Esse número de telefone já está em uso."
+          },
+          {
+            status: 409
+          }
+        );
+      }
+    }
+
+    await prisma.employee.update({
+      where: {
+        id
+      },
+      data: {
+        name,
+        role,
+        phone,
+        entryDate: entryDate
+          ? parse(entryDate, "dd/MM/yyyy", new Date())
+          : undefined,
+        salary: salary ? Math.round(salary * 100) : undefined
+      }
+    });
+
+    return new NextResponse(null, {
+      status: 204
+    });
+  } catch (error: any) {
+    if (error instanceof ZodError) {
+      return NextResponse.json(
+        {
+          message: "Validation error.",
+          errors: error.flatten().fieldErrors
+        },
+        {
+          status: 400
+        }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        message: error.message
+      },
+      {
+        status: 500
+      }
+    );
+  }
+}
