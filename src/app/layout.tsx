0a7f581796e6850a8408c3828f3f50f82f0aab16
Caio Vinícius <caio1525pereira@gmail.com>
2024-06-26 02:13:19 -0300

    chore: :wrench: Adição de propriedade requerida do next themes na tag html (suppressHydrationWarning )

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" suppressHydrationWarning>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
