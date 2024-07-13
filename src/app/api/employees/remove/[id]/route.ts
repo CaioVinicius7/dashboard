66c40615d65b2f057ca095bcc3b0e579a277ce59
Caio Vinícius <caio1525pereira@gmail.com>
2024-07-13 17:22:05 -0300

    refactor: :recycle: Retorno do endpoint de remoção de funcionário

--- src/app/api/employees/remove/[id]/route.ts
+++ src/app/api/employees/remove/[id]/route.ts
@@ -38,7 +38,7 @@ export async function DELETE(_: NextRequest, { params }: { params: Params }) {
       }
     });
 
-    return new NextResponse(null, {
+    return new NextResponse(undefined, {
       status: 204
     });
   } catch (error: any) {
