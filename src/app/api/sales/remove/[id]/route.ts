30a39e44b360995877d34e91f86e22687d4cd95c
Caio Vinícius <caio1525pereira@gmail.com>
2024-07-13 17:20:01 -0300

    feat: :sparkles: Endpoint de remoção de venda

---
+++ src/app/api/sales/remove/[id]/route.ts
@@ -0,0 +1,66 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { z, ZodError } from "zod";
+
+import { prisma } from "@/lib/prisma";
+
+interface Params {
+  id: string;
+}
+
+const removeSaleParamsSchema = z.object({
+  id: z.string()
+});
+
+export async function DELETE(_: NextRequest, { params }: { params: Params }) {
+  try {
+    const { id } = removeSaleParamsSchema.parse(params);
+
+    const saleFromId = await prisma.sale.findUnique({
+      where: {
+        id
+      }
+    });
+
+    if (!saleFromId) {
+      return NextResponse.json(
+        {
+          message: "Venda não encontrada."
+        },
+        {
+          status: 400
+        }
+      );
+    }
+
+    await prisma.sale.delete({
+      where: {
+        id
+      }
+    });
+
+    return new NextResponse(undefined, {
+      status: 204
+    });
+  } catch (error: any) {
+    if (error instanceof ZodError) {
+      return NextResponse.json(
+        {
+          message: "Validation error.",
+          errors: error.flatten().fieldErrors
+        },
+        {
+          status: 400
+        }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        message: error.message
+      },
+      {
+        status: 500
+      }
+    );
+  }
+}
