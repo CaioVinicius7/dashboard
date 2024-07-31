72cf748693455503ce380092bc4348ea6e6ed9c5
Caio Vinícius <caio1525pereira@gmail.com>
2024-07-31 16:39:34 -0300

    feat: :sparkles: Endpoint de listagem do valor total das vendas do mês

---
+++ src/app/api/metrics/month-sales-amount/route.ts
@@ -0,0 +1,70 @@
+import { endOfMonth, startOfMonth, subMonths } from "date-fns";
+import { NextResponse } from "next/server";
+
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  try {
+    const now = new Date();
+
+    const startOfCurrentMonth = startOfMonth(now);
+    const endOfCurrentMonth = endOfMonth(now);
+
+    const previousMonth = subMonths(now, 1);
+
+    const startOfPreviousMonth = startOfMonth(previousMonth);
+    const endOfPreviousMonth = endOfMonth(previousMonth);
+
+    const [currentMonthSalesAmount, previousMonthSalesAmount] =
+      await prisma.$transaction([
+        prisma.sale.aggregate({
+          _sum: {
+            value: true
+          },
+          where: {
+            dateOfSale: {
+              gte: startOfCurrentMonth,
+              lte: endOfCurrentMonth
+            }
+          }
+        }),
+        prisma.sale.aggregate({
+          _sum: {
+            value: true
+          },
+          where: {
+            dateOfSale: {
+              gte: startOfPreviousMonth,
+              lte: endOfPreviousMonth
+            }
+          }
+        })
+      ]);
+
+    const currentTotal = currentMonthSalesAmount._sum.value ?? 0;
+    const previousTotal = previousMonthSalesAmount._sum.value ?? 0;
+
+    const diffFromPreviousMonth = currentTotal - previousTotal;
+
+    const response = NextResponse.json(
+      {
+        monthSalesAmount: currentTotal,
+        diffFromPreviousMonth
+      },
+      {
+        status: 200
+      }
+    );
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: error.message
+      },
+      {
+        status: 500
+      }
+    );
+  }
+}
