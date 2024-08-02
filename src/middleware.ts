13983a3fa17e7a115dfc1af1af084c798919aeb4
Caio Vinícius <caio1525pereira@gmail.com>
2024-08-02 19:57:32 -0300

    feat: :passport_control: Protegendo rotas de métricas

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,5 +26,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/api/employees/:path*", "/api/sales/:path*"]
+  matcher: ["/api/employees/:path*", "/api/sales/:path*", "/api/metrics/:path*"]
 };
