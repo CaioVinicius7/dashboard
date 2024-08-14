c169b2adf6d965a642b204a0e5f0d61fb7b9787a
Caio Vinícius <caio1525pereira@gmail.com>
2024-08-13 21:43:40 -0300

    feat: :children_crossing: Loading screen da página de despesas

---
+++ src/app/(dashboard)/expenses/loading.tsx
@@ -0,0 +1,76 @@
+import { ArrowDownNarrowWide } from "lucide-react";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from "@/components/ui/table";
+
+export default function Loading() {
+  return (
+    <>
+      <header className="flex items-center justify-between border-b p-4">
+        <div className="flex items-center gap-2">
+          <ArrowDownNarrowWide />
+
+          <h2 className="text-xl">Despesas</h2>
+        </div>
+
+        <Skeleton className="size-10 rounded-md" />
+      </header>
+
+      <main className="space-y-4 p-4">
+        <div className="flex items-center justify-between">
+          <Skeleton className="h-10 w-[102px]" />
+
+          <Skeleton className="h-10 w-32" />
+        </div>
+
+        <Table className="min-w-[1000px]">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="min-w-[175px]">Título</TableHead>
+              <TableHead className="w-[175px]">valor</TableHead>
+              <TableHead className="w-[175px]">Data do ocorrido</TableHead>
+              <TableHead className="w-[175px]">Data de registro</TableHead>
+              <TableHead className="w-[175px]">Data de atualização</TableHead>
+              <TableHead className="w-[150px]" />
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {Array.from({ length: 6 }).map((_, i) => (
+              <TableRow key={i}>
+                <TableCell className="font-medium">
+                  <Skeleton className="h-5 w-72" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-5 w-16" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-5 w-28" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-5 w-20" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-5 w-20" />
+                </TableCell>
+
+                <TableCell className="flex items-center gap-2">
+                  <Skeleton className="h-10 w-10" />
+
+                  <Skeleton className="h-10 w-10" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </main>
+    </>
+  );
+}
