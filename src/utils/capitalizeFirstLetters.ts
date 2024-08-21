f712952eada31b3047ec68826b0fd67656c9efb5
Caio Vinícius <caio1525pereira@gmail.com>
2024-08-20 22:47:07 -0300

    refactor: :recycle: Alterando função capitalizeFirstLetters

--- src/utils/capitalizeFirstLetters.ts
+++ src/utils/capitalizeFirstLetters.ts
@@ -1,6 +1,10 @@
 export function capitalizeFirstLetters(text: string) {
   return text
     .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .map((word) =>
+      word.length > 2
+        ? word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        : word
+    )
     .join(" ");
 }
