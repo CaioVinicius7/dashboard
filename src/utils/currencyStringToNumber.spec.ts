95db18827e465531b276e84517d8f3d66e63786f
Caio Vinícius <caio1525pereira@gmail.com>
2024-08-27 02:59:12 -0300

    test: :white_check_mark: Adição de testes da função currencyStringToNumber

--- src/utils/currencyStringToNumber.spec.ts
+++ src/utils/currencyStringToNumber.spec.ts
@@ -6,4 +6,34 @@ describe("currencyStringToNumber", () => {
 
     expect(currencyStringToNumber(currencyString)).toEqual(1721.37);
   });
+
+  it("Should be able to convert a currency string without dots", () => {
+    const currencyString = "R$1721,37";
+
+    expect(currencyStringToNumber(currencyString)).toEqual(1721.37);
+  });
+
+  it("Should be able to return the input if it's already a number", () => {
+    const value = 1721.37;
+
+    expect(currencyStringToNumber(value)).toEqual(1721.37);
+  });
+
+  it("Should be able to convert a currency string with no cents", () => {
+    const currencyString = "R$1.721";
+
+    expect(currencyStringToNumber(currencyString)).toEqual(1721);
+  });
+
+  it("Should be able to convert a currency string with spaces around the value", () => {
+    const currencyString = " R$ 1.721,37 ";
+
+    expect(currencyStringToNumber(currencyString.trim())).toEqual(1721.37);
+  });
+
+  it("Should be able to convert a currency string with no thousands separator", () => {
+    const currencyString = "R$1721";
+
+    expect(currencyStringToNumber(currencyString)).toEqual(1721);
+  });
 });
